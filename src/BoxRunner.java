f173f2167d315ba0ac1efcedbf5b59edbcd74187
student <sample@bths.edu>
2022-09-30 12:31:23 -0400

    jfalkdsjs

--- src/BoxRunner.java
+++ src/BoxRunner.java
@@ -7,6 +7,21 @@ public class BoxRunner {
         //Box box3 = new Box(2.5, 6.7);
         //Box bo4 = new Box();
 
+        Box newBox = new Box(6, 10, 8);
+
+        double larger = newBox.anySideLongerThan(10); // A
+
+        boolean big = newBox.anySideLongerThan(7.5); // B
+
+        boolean x = newBox.anySideLongerThan(5); // C
+
+        String dim = newBox.printDimensions(); // D
+
+        System.out.println(newBox.printDimensions()); // E
+
+        newBox.printDimensions(); // F
+
+        double volume = newBox.volume(21.8); // G
     }
 
 }
